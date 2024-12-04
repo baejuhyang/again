3473f54779b74294066596369655283dd0bf9a1a
baejuhyang <juhyang9897@gmail.com>
2024-12-04 10:10:52 +0900

    04/Dec/2024

--- 03-js/10_arrayLoop.js
+++ 03-js/10_arrayLoop.js
@@ -33,39 +33,40 @@ for (let a = 0; a < arr.length; a++) {
 // const alphabets = ['a', 'b', 'c', 'd'];
 
 // for (let alpha of alphabets) {
-//     console.log(`alpha >>> ${alpha}`);
+//     console.log(alpha);
 // }
+
 // - 배열 'alphabets'의 각 요소를 반복하면서 'alpha'변수에 순서대로 할당하고,
 // 각각 요소에 대해 반복문 블록 실행.
 // - alphabets: 배열, 문자열 등등.. 반복 가능한 객체
-// - alpha: 반복되는 각 항목을 의미. 각 항목에 대해 한 번씩 실행 (=개발자 정의 변수_
+// - alpha: 반복되는 각 항목을 의미. 각 항목에 대해 한 번씩 실행 (=개발자 정의 변수)
 //     - 다만, 변수명이 반복하는 값을 잘 표현할 수 있도록 적절하게 설정하는 것이 좋다.
 
 // #3. forEach() 메서드
-// - 배열의 각 요소에 대해 지정된 콜백 함수르 실행.
+// - 배열의 각 요소에 대해 지정된 콜백 함수를 실행.
 // - 배열 메소드라 배열에서만 사용 가능.
 // - 일반적으로 배열을 순회하면서 각 요소에 대해 동일한 작업을 수행할 때 사용.
 // - 각 요소에 대해 함수가 호출 될 때마다, 해당 요소가 함수의 인자로 전달된다.
 
-const alphabets = ['a', 'b', 'c', 'd'];
+// const alphabets = ['a', 'b', 'c', 'd'];
 
-alphabets.forEach(function (alpha) {
-    // alpha: currentvalue를 의미 = 반복하고 있는 현재 요소
-    console.log('element: ', alpha);
-});
+// alphabets.forEach(function (alpha) {
+//     // alpha: currentvalue를 의미 = 반복하고 있는 현재 요소
+//     console.log('element: ', alpha);
+// });
 
-alphabets.forEach(function (alpha, index) {
-    // alpha: currentvalue를 의미 = 반복하고 있는 현재 요소
-    // index: currentvalue의 index(위치)
-    console.log(`element: ${alpha}, Index: ${index}`);
-});
+// alphabets.forEach(function (alpha, index) {
+//     // alpha: currentvalue를 의미 = 반복하고 있는 현재 요소
+//     // index: currentvalue의 index(위치)
+//     console.log(`element: ${alpha}, Index: ${index}`);
+// });
 
-alphabets.forEach(function (alpha, index, array) {
-    // alpha: currentvalue를 의미 = 반복하고 있는 현재 요소
-    // index: currentvalue의 index(위치)
-    // array: forEach를 호출한 배열
-    console.log(`element: ${alpha}, Index: ${index}, Array: ${array}`);
-});
+// alphabets.forEach(function (alpha, index, array) {
+//     // alpha: currentvalue를 의미 = 반복하고 있는 현재 요소
+//     // index: currentvalue의 index(위치)
+//     // array: forEach를 호출한 배열
+//     console.log(`element: ${alpha}, Index: ${index}, Array: ${array}`);
+// });
 
 //  ex) forEach 사용
 num = [1, 3, 5, 9];
@@ -94,18 +95,18 @@ console.log(num);
 // #1. map()
 // - 각 배열의 각 요소에 대해 지정된 함수를 호출(실행)히고, 그 결과로 **새로운 배열**을 생성.
 // 새로운 배열을 생성하여 기존의 원본 배열은 변하지 않는다.
-// const numbers = [1, 2, 3, 4, 5];
-// const doublueNumbers = numbers.map(function (number) {
-//     return number * 2;
-// });
-// console.log(doublueNumbers);
+const numbers = [1, 2, 3, 4, 5];
+const doublueNumbers = numbers.map(function (number) {
+    return number * 2;
+});
+console.log(doublueNumbers);
 
 // +) 추가 설명
 // 구체적으로 콜백 함수는 map() 메서드에 인수로 전달
 // 각 요소를 처리하는 함수로서 배열의 각 요소를 순회할 때마다 호출됨.
 // 이 콜백 함수는 배열의 각 요소를 인자로 받아서 원하는 작업을 수행하고 그 결과를 반환.
 
-// num은 콜백 함수의 매개변수이다.
+// number는 콜백 함수의 매개변수이다.
 // = 이는 현재 처리중인 배열의 요소를 나타낸다.
 // 이 매개변수를 활용하여 각 요소에 대해 원하는 작업을 수행하는 것이다.
 
@@ -125,35 +126,35 @@ console.log(num);
 // - 배열의 요소를 하나씩 검사하고, 조건을 만족하는 첫 번째 요소를 찾으면 그 즉시 반복 종료.
 // - 조건에 만족 -> 해당 요소 반환
 // - 조건에 불만족 -> undefined 반환
-const numbers = [1, 2, 3, 4, 5];
+// const numbers = [1, 2, 3, 4, 5];
 // const pracNumbers2 = numbers.find(function (num) {
 //     return num % 2 === 0;
 // });
 // console.log(pracNumbers2); // 2 // 해당 요소 하나만 반환함.
 
-const pracNumbers2 = numbers.find(function (num) {
-    return num % 6 === 0;
-});
-console.log(pracNumbers2); // undefined // 조건에 만족하는 요소 없음.
+// const pracNumbers2 = numbers.find(function (num) {
+//     return num % 6 === 0;
+// });
+// console.log(pracNumbers2); // undefined // 조건에 만족하는 요소 없음.
 
 // +) 객체에서 반복문 사용하기
 // for ... in 반복문
 // 객체의 'key'를 반복할 수 있는 구문
 
-const dog = {
-    // 속성을 반복할 객체
-    name: '태풍',
-    gender: 'M',
-    info: 'cute',
-};
-console.log(dog.name);
-console.log(dog['name']); // 위와 같다.
+// const dog = {
+//     // 속성을 반복할 객체
+//     name: '태풍',
+//     gender: 'M',
+//     info: 'cute',
+// };
+// console.log(dog.name);
+// console.log(dog['name']); // 위와 같다.
 
-console.log(dog.gender);
-console.log(dog.info);
+// console.log(dog.gender);
+// console.log(dog.info);
 
-for (let k in dog) {
-    console.log(`key: ${k}, value: ${dog[k]}`); // 객체의 각 속성(key, value)에 대해 실행할 코드
-    // k: key를 의미 - 각 속성의 키를 저장하는 변수 (사요아 정의 변수)
-    // dog[k]: value를 의미
-}
+// for (let k in dog) {
+//     console.log(`key: ${k}, value: ${dog[k]}`); // 객체의 각 속성(key, value)에 대해 실행할 코드
+//     // k: key를 의미 - 각 속성의 키를 저장하는 변수 (사요아 정의 변수)
+//     // dog[k]: value를 의미
+// }
